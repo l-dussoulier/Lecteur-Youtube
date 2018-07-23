b4f8f54244c8182e3eb893d382bfe1745e7ec19e
DUSSOULIER <dussoulier.louis@hotmail.fr>
2018-07-23 18:48:02 +0200

    Ajout de la tableView et création du modele

---
+++ Lecteur Youtube/TableauController.swift
@@ -0,0 +1,54 @@
+//
+//  TableauController.swift
+//  Lecteur Youtube
+//
+//  Created by DUSSOULIER on 13/07/2018.
+//  Copyright © 2018 DUSSOULIER. All rights reserved.
+//
+
+import UIKit
+
+class TableauController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+   
+    var chansons = [Chanson]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        ajouterChanson()
+    }
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chansons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+    
+    func ajouterChanson() {
+        chansons = [Chanson]()
+        let cool = Chanson(artiste: "Casseur Flowters", titre: "Ils sont cools", code: "_DT-4-jJTZc")
+        chansons.append(cool)
+        let bien = Chanson(artiste: "Orelsan", titre: "Tout va bien", code: "dq6G2YWoRqA")
+        chansons.append(bien)
+        let basique = Chanson(artiste: "Orelsan", titre: "Basique", code: "2bjk26RwJyU")
+        chansons.append(basique)
+        let ronde = Chanson(artiste: "Orelsan", titre: "La terre est ronde", code: "oGdhZyS2ozo")
+        chansons.append(ronde)
+        let san = Chanson(artiste: "Orelsan", titre: "San", code: "PejyoeG_Tma")
+        chansons.append(san)
+        let seul = Chanson(artiste: "Orelsan", titre: "Si seul", code: "cz-PLtXh7Lo")
+        chansons.append(seul)
+        let inacheves = Chanson(artiste: "Casseur Flowters", titre: "Inachevéq", code: "MW1eQZ_PEb4")
+        chansons.append(inacheves)
+        tableView.reloadData()
+        
+        
+    }
+}
